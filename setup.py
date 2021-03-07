f635713e4e8c63179198b294c561c073fc7d3b03
Gilles Vandelle <gilles.vandelle@kelkoo.com>
2021-03-07 09:15:20 +0100

    train notebook added

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+    name='whiteboard',
+    version='1.0',
+    packages=['whiteboard'],
+    url='',
+    license='',
+    author='GV',
+    author_email='',
+    description='Collaborative whiteboard with shape recognition',
+    install_requires=["asyncio","aiohttp","numpy","tensorflow"]
+)
+
+
